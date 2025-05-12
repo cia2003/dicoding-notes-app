240e586ffe32300e2731d1796ee70dada0286934
Gracia Naimora Samosir <137704190+cia2003@users.noreply.github.com>
2025-05-12 13:23:07 +0800

    Update index.js

--- src/scripts/components/index.js
+++ src/scripts/components/index.js
@@ -1,6 +1,6 @@
 import './note-item.js';
 import './nav-bar.js';
-// import './add-note-form.js';
+import './add-note-form.js';
 import './add-note-form/add-note-form.js'
 import './search-bar.js';
 import './attribution.js';
