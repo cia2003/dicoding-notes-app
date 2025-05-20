c1adc944d433822261eb17bf35d79139f09fa1ab
Gracia Naimora Samosir <137704190+cia2003@users.noreply.github.com>
2025-05-20 10:08:40 +0800

    Delete src directory

--- src/scripts/components/index.js
+++
@@ -1,7 +0,0 @@
-import "./note-item/note-item.js";
-import "./nav-bar/nav-bar.js";
-import "./add-note-form/add-note-form.js";
-import "./search-bar.js";
-import "./attribution/attribution.js";
-import "./form-dialog/form-dialog.js";
-import "./loading-indicator/loading-indicator.js";
--- src/scripts/features/notes/notes-index.js
+++
@@ -1,4 +0,0 @@
-export * from "./notes-dialog.js";
-export * from "./notes-event.js";
-export * from "./notes-operation.js";
-export * from "./notes-ui.js";
